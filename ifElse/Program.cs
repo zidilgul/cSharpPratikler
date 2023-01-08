22aac3932c1323b5a19e7f387b7501e1b4c61d63
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-08 23:10:01 +0300

    if else lesson codes

---
+++ ifElse/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+        private static void Main(string[] args)
+        {
+                int time = DateTime.Now.Hour;
+
+                if(time > 18)
+                System.Console.WriteLine("İyi akşamlar");
+                else if(time > 12)
+                System.Console.WriteLine("İyi günler");
+                else
+                {
+                        System.Console.WriteLine("İyi sabahlar");
+                }
+        }
+}
