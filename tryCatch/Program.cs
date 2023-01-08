fa35bce2cb702f876e1e00856b57dc54f0bb63ef
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-08 22:51:55 +0300

    try catch done

--- tryCatch/Program.cs
+++ tryCatch/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+        private static void Main(string[] args)
+        {
+
+                try{  
+                        Console.WriteLine("Bir sayı giriniz: ");
+                        int sayi = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Sayınız = " + sayi);
+                }
+                catch(Exception exc){
+                        Console.WriteLine(exc.Message);  
+
+                }
+                finally{
+                        System.Console.WriteLine("program sonlandı");
+
+                }
+              }
+}
