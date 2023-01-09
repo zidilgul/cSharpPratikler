60773d2febbc6aa256a9b332b00ddc1851eb060b
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-09 22:53:45 +0300

    forEach and while lesson codes

--- whileAndForEach/Program.cs
+++ whileAndForEach/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //1den başlayarak consoledan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            //a dan z ye kadar tüm harfleri console a yazdır.
+            char character = 'a';
+            while(character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            Console.WriteLine("Foreach");
+            //FOREACH
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
